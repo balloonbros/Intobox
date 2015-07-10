8175492072197c849236d9eabe2f9488f6ee72af
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-07-10 15:27:32 +0900

    xmpp_loginは廃止されたので削除

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.oauth.facebook.app, Rails.application.secrets.oauth.facebook.secret,
-           :scope => 'email,xmpp_login',
+           :scope => 'email',
            :callback_url => Settings.oauth.facebook.redirect_uri
 
   provider :dropbox_oauth2, Rails.application.secrets.oauth.dropbox.app, Rails.application.secrets.oauth.dropbox.secret,
